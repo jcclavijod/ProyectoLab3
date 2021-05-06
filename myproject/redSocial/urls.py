d03fed71f935c8b7a4d38f664e10ff6e5bec2ff4
Juan Camilo Clavijo <clavijojuancamilo@gmail.com>
2021-05-05 22:09:24 -0500

    Se crean las primeras paginas html que son: paginaInicio, vistaPost y perfil

---
+++ myproject/redSocial/urls.py
@@ -0,0 +1,1 @@
+from django.urls import pathfrom . import viewsurlpatterns = [    path('',views.vistaPost, name='vistaPost'),    path('perfil/',views.perfil, name='perfil'),]
--- myproject/redSocial/views.py
+++ myproject/redSocial/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def vistaPost(request):
+    return render(request,'paginas/vistaPost.html')
+
+def perfil(request):
+    return render(request,'paginas/perfil.html')
